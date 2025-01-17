88e243d8adf77468b23cf86da7b5e1ec140515ba
Robert Parcus <923630+rparcus@users.noreply.github.com>
2025-01-17 11:35:05 +0900

    feat: update env variable files

--- config/runtime.exs
+++ config/runtime.exs
@@ -5,6 +5,15 @@ if config_env() == :test do
   source(["test.envrc", "test.override.envrc"])
 else
   source(["#{config_env()}.envrc", "#{config_env()}.override.envrc", System.get_env()])
+<<<<<<< HEAD
+=======
+end
+
+if config_env() in [:dev, :test] do
+  config :lux, :api_keys,
+    alchemy: env!("ALCHEMY_API_KEY"),
+    openai: env!("OPENAI_API_KEY")
+>>>>>>> 43c7337 (feat: update env variable files)
 end
 
 config :lux, :api_keys,
